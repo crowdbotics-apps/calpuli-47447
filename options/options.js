0362380a4c7d62292110876740589e207c409388
Crowdbotics <team@crowdbotics.com>
2024-04-10 17:16:50 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "calpuli-47447",
+  url: "https://calpuli-47447.botics.co",
+  api: "https://calpuli-47447.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
